50b3089ba578fcc2ef33f0d8002e1e31ace7c294
IMP1 <IMP1@mail.com>
2016-05-03 10:11:34 +0100

    asking for confirmation for actions is now in the settings (cos its long)

--- src/run/Settings.java
+++ src/run/Settings.java
@@ -3,8 +3,14 @@ package run;
 public final class Settings {
 	private Settings() {}
 	
-	public static boolean showTurnAnimations = true;
-	public static boolean showMoveAnimations = true;
+	public static boolean showTurnAnimations   = true;
+	public static boolean showMoveAnimations   = true;
 	public static boolean showAttackAnimations = true;
+	
+	public static boolean askConfirmationOnMove   = true;
+	public static boolean askConfirmationOnLoad   = true;
+	public static boolean askConfirmationOnUnload = true;
+	public static boolean askConfirmationOnAttack = true;
+	public static boolean askConfirmationOnDefend = true;
 
 }
