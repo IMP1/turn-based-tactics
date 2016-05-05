c150a8552886113c7f24ad73333c08e335d9249c
IMP1 <IMP1@mail.com>
2016-05-05 23:53:50 +0100

    defending is done except for making the unit's state change

--- src/scn/battleState/Defend.java
+++ src/scn/battleState/Defend.java
@@ -1,37 +1,53 @@
 package scn.battleState;
 
-import cls.unit.Unit;
 import run.Settings;
 import scn.Battle;
 import scn.Battle.State;
 
 public class Defend extends State {
 	
-	private int targetX;
-	private int targetY;
+	private int direction;
 
 	public Defend(Battle scene) {
 		super(scene);
-		unit = null;
-		targetX = -1;
-		targetY = -1;
+		direction = -1;
 	}
 
 	@Override
 	public void mouseClick(int x, int y, int i, int j) {
-		if ((targetX == i && targetY == j) || !Settings.askConfirmationOnDefend) {
+		if ((direction == getDirection(i, j)) || !Settings.askConfirmationOnDefend) {
 			// TODO make unit defend in that direction
+			System.out.println("Defending!");
 			hideDefendableDirections();
 			setNextState(new Idle(scene));
-		} else if (false /* TODO  */) {
-				targetX = i;
-				targetY = j;
-			}
+		} else if (validClick(i, j)) {
+			direction = getDirection(i, j);
+		} else if (direction > -1) {
+			direction = -1;
+		} else {
+			cancel();
 		}
+	}
+	
+	private boolean validClick(int i, int j) {
+		return true;
+	}
+	
+	private int getDirection(int i, int j) {
+		int[] pos = getSelectedPosition();
+		int x = pos[0]; 
+		int y = pos[1];
+		double r = Math.atan2(j - y, i - x);
+		if (r < 0) r += 2 * Math.PI;
+		return (int)(r * 4 / Math.PI);
 	}
 
 	@Override
 	public void cancel() {
+		if (direction != -1) {
+			direction = -1;
+			return;
+		}
 		unsetAction();
 		hideDefendableDirections();
 		setNextState(new ActionWheel(scene));
@@ -39,7 +55,7 @@ public class Defend extends State {
 
 	@Override
 	public void drawMap() {
-		scene.drawDefendDirections();
+		scene.drawDefendDirections(direction);
 	}
 
 	@Override
