3c7930b6eb164e400002ac30f11e46de56636e6a
Huw Taylor <huw_taylor@hotmail.co.uk>
2016-10-16 22:37:14 +0100

    started on battle logging

---
+++ src/cls/level/BattleLog.java
@@ -0,0 +1,43 @@
+package cls.level;
+
+import java.util.ArrayList;
+
+import cls.unit.Unit;
+
+public class BattleLog {
+
+	public enum ActionType {
+		UNIT_MOVE,
+		UNIT_LOAD,
+		UNIT_BATTLE,
+		TURN_END,
+	}
+	
+	private class Action {
+		public final ActionType actionType;
+		public final Object[]   params;
+		
+		private Action(ActionType type, Object... args) {
+			actionType = type;
+			params = args;
+		}
+	}
+	
+	private int startingPlayer;
+	private ArrayList<Action> log;
+	
+	protected BattleLog(int startingPlayer) {
+		this.startingPlayer = startingPlayer;
+		this.log = new ArrayList<Action>();
+	}
+	
+	public void unitMove(Unit u, int oldX, int oldY, int newX, int newY) {
+		Action a = new Action(ActionType.UNIT_MOVE, oldX, oldY, u.name, newX, newY);
+		log.add(a);
+	}
+
+	public void unitLoad(Unit u, int oldX, int oldY, Unit transport, int tx, int ty) {
+		Action a = new Action(ActionType.UNIT_LOAD, oldX, oldY, u.name, tx, ty, transport.name);
+	}
+	
+}
